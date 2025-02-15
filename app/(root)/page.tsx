f3da9e263a1bb3d807c520315c172e73866de3f3
Your Name <you@example.com>
2025-02-15 16:00:56 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/ui/HeaderBox'
+import { Section } from 'lucide-react'
 import React from 'react'
 
 const Home= () => {
+const loggedIn ={firstName: 'Aditya', lastName:
+   'krishna',email:'venkatakrishnaaditya2003@gmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName ||'Guest'}
+          subtext="Acesses and manage your Account and transactions efficiently" 
+          />
+
+          <TotalBalanceBox
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 123.50},{currentBalance: 600.78}]}
+      />
+    </section>
   )
 }
 
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,34 @@
+
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
+
+const TotalBalanceBox = ({
+    accounts =[], totalBanks, totalCurrentBalance
+}: TotalBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+        <div className="total-balance-chart">
+          <DoughnutChart accounts={accounts}/>
+        </div>
+        <div className="flex flex-col gap-6">
+            <h2 className="header-2">
+               Bank Accounts: {totalBanks}
+            </h2>
+            <div className="flex flex-col gap-2">
+              <p className="total-balance-label">
+                Total Current Balance
+
+              </p>
+              <div className="total-balance-amoount flex-center gap-2 text-2xl font-extrabold text-black">
+                <AnimatedCounter amount=
+                {totalCurrentBalance}/>
+                
+              </div>
+            </div>
+
+        </div>
+   </section>     
+  )
+}
+
+export default TotalBalanceBox
